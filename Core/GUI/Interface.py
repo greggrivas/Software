12ce651519efda48e7dc716f1b4ea57c36e6a5f9
ggrivas <ggrivas@physics.auth.gr>
2021-11-12 12:18:29 +0200

    added tools file

--- Core/GUI/Interface.py
+++ Core/GUI/Interface.py
@@ -5,18 +5,20 @@ import logging
 from functools import partial
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 
+#from Core.GUI.interface_tests import find_fits_files
+
 class UISoftware(QtWidgets.QApplication):
 
     def __init__(self, parent=None):
         super(UISoftware, self).__init__(parent)
         self.logger = logging.getLogger(__name__) # Create the logger for the file
 
-
         # Create the main GUI window and the other windows
         self.main_window = QtWidgets.QMainWindow()
         self.ui_red_settings_window = QtWidgets.QMainWindow(parent=self.main_window)  # Create reduction target and settings window
         self.ui_man_target_window = QtWidgets.QMainWindow(parent=self.main_window) # Create reduction manual target window
-
+        
+        #Loading the ui files
         try:
             self.main_widget = uic.loadUi(r'C:\Users\PC-CP\Documents\Projects\Software\ui_files\main_menu.ui', self.main_window)
             self.red_set_win = uic.loadUi(r'C:\Users\PC-CP\Documents\Projects\Software\ui_files\reduction_settings_target.ui', self.ui_red_settings_window)
@@ -33,6 +35,7 @@ class UISoftware(QtWidgets.QApplication):
         # TARGET AND SETTINGS WINDOW
         self.red_set_win.target_manually_btn.clicked.connect(self.manual_target_win.show)
         self.red_set_win.open_dir_tool.clicked.connect(lambda: self.get_directory_dialog())
+        #self.red_set_win.Nbias_found.setText(self.update_fits_files('test'))
 
 
     def get_directory_dialog(self):
@@ -41,10 +44,12 @@ class UISoftware(QtWidgets.QApplication):
         self.red_set_win.directory_path_qline.setText(self.directory_path)
     
     def find_fits_files(self, identifier):
-        # FINDS FITS FILE BASED ON THE IDENTIFIER PROVIDED
-        if len(identifier) > 0:
+        # FINDS FITS FILES BASED ON THE IDENTIFIER PROVIDED
+        if len(self.identifier) > 0:
             self.fits_files = glob.glob('{0}.fits'.format(self.identifier))
-            #self.fits_files = 
+            self.fits_files = list(self.fits_files)
+            self.fits_files_length = len(self.fits_files)
+            return self.fits_files_length
 
 
 
